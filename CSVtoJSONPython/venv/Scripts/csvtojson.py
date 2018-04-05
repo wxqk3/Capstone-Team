5d9baa59b5661a876ad38a2f09a2489b284e9642
sbwzq8 <sbwzq8@mail.missouri.edu>
2018-04-05 15:47:42 -0500

    redid parser to upload to firebase based on username

--- CSVtoJSONPython/venv/Scripts/csvtojson.py
+++ CSVtoJSONPython/venv/Scripts/csvtojson.py
@@ -1,16 +1,54 @@
 import csv
-import json
-import pprint
+# import json
 from firebase import firebase
+from time import gmtime, strftime, sleep
 
+# Create dictionary of key/value pairs from file
 readFile = csv.DictReader(open('../../test.csv', 'r'))
 dictionary = []
-i = 0
+
+MaxTime = 0
+# Load all lines into dictionary
 for line in readFile:
-    dictionary.append(line)
+    if MaxTime > 1000:
+        break
+    else:
+        dictionary.append(line)
+    MaxTime = MaxTime+1
+# Take every tenth dictionary entry for every tenth of a second
+wantedTimes = dictionary[0::10]
 
-with open('../../test.json', 'w') as writeFile:
-    json.dump(dictionary, writeFile, indent=2)
+print("Done reading at MaxTime =" + str(MaxTime))
 
-firebase = firebase.FirebaseApplication('https://telemetryapp-16f5d.firebaseio.com/')
-result = firebase.post('user/Sean', dictionary)
+# Create timestamp for run upload
+runStartTime = strftime("%m-%d-%Y %H:%M:%S", gmtime())
+
+# open connection to database
+url = "https://telemetryapp-16f5d.firebaseio.com"
+fb = firebase.FirebaseApplication(url, None)
+
+# username and path to user for database
+username = "sbwzq8"
+userPath = "user/" + username + "/runs/"
+
+
+# handle updating current run
+for values in wantedTimes:
+    # result = fb.put('SeansPlayground/Sean/', 'Current Run', values)
+    result = fb.put(userPath, 'Current Run', values)
+    sleep(.01)
+
+# save run under folder with start time
+result = fb.put(userPath, runStartTime, wantedTimes)
+
+# After 10 seconds remove current run
+sleep(10)
+result = fb.delete(userPath, 'Current Run')
+
+
+
+
+# deprecated, don't need json
+# with open('../../test.json', 'w') as writeFile:
+#     json.dump(wantedTimes, writeFile, indent=2)
+#     print("Dumping Data")
