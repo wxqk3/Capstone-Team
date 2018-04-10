bdfae1c6d0d6bb99be6d15d82bad8e82fb764381
sbwzq8 <sbwzq8@mail.missouri.edu>
2018-04-10 15:18:02 -0500

    Modified Parser

--- CSVtoJSONPython/venv/Scripts/csvtojson.py
+++ CSVtoJSONPython/venv/Scripts/csvtojson.py
@@ -2,22 +2,31 @@ import csv
 # import json
 from firebase import firebase
 from time import gmtime, strftime, sleep
+import collections
 
 # Create dictionary of key/value pairs from file
 readFile = csv.DictReader(open('../../test.csv', 'r'))
 dictionary = []
 
-# MaxTime = 0
+MaxTime = 0
 # Load all lines into dictionary
 for line in readFile:
-    # if MaxTime > 160000:
-    #     break
-    # else:
-    dictionary.append(line)
-    # MaxTime = MaxTime+1
-# Take every tenth dictionary entry for every tenth of a second
-wantedTimes = dictionary[0::10]
+    if MaxTime < 500:
+        dictionary.append(line)
+        # print(dictionary[MaxTime])
+        dictionary[MaxTime]["Time(ms)"] = str(int(float(dictionary[MaxTime]["Time(ms)"]) * 1000))
+        MaxTime = MaxTime+1
+    else:
+        break
 
+# for entry in dictionary:
+#     print(entry)
+
+# Take every tenth dictionary entry for every tenth of a second
+# wantedTimes = dictionary[0::10]
+wantedTimes = dictionary
+wantedTimes = range(dictionary[500], dictionary[1000])
+# print(dictionary)
 # print("Done reading at MaxTime =" + str(MaxTime))
 
 # Create timestamp for run upload
@@ -28,24 +37,30 @@ url = "https://telemetryapp-16f5d.firebaseio.com"
 fb = firebase.FirebaseApplication(url, None)
 
 # username and path to user for database
-username = "sbwzq8"
+username = "aptyt7"
 userPath = "user/" + username + "/runs/"
 
 liveData = False
 
 # handle updating current run
-if liveData == True:
+if liveData:
+    i = 0
     for values in wantedTimes:
         # result = fb.put('SeansPlayground/Sean/', 'Current Run', values)
-        result = fb.put(userPath, 'Current Run', values)
-        sleep(.01)
+        result = fb.put(userPath, 'Current Run/'+str(i), values)
+
+        i = i+1
+        sleep(.1)
 
 # save run under folder with start time
-result = fb.put(userPath, runStartTime, wantedTimes)
+# result = fb.put(userPath, runStartTime, wantedTimes)
+
 # print(runStartTime)
 
-# # After 10 seconds remove current run from database
-if liveData == True:
+# fb.delete(userPath, '')
+
+# After 10 seconds remove current run from database
+if liveData:
     sleep(10)
     result = fb.delete(userPath, 'Current Run')
 
